2cc9b52d99ac7fdcb18ce1faf5f7e67bc1e1b26b
krisjanis.bergmanis <krisjanis.bergmanis.external@kneip.com>
2018-07-30 23:05:32 +0300

    First feature

---
+++ src/test/java/testrunner/TestRunner.java
@@ -0,0 +1,14 @@
+package testrunner;
+
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions"}
+)
+public class TestRunner {
+    
+}
